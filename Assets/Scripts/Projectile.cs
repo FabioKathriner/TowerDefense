47887545f48fca5c795c5b0287b630d27b304471
TedMANNERs <mannersofted@gmail.com>
2019-11-09 18:45:12 +0100

    Destroy projectile after 5 seconds

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,7 +7,7 @@ namespace Assets.Scripts
         // Start is called before the first frame update
         void Start()
         {
-        
+            Destroy(gameObject, 5f);
         }
 
         // Update is called once per frame
