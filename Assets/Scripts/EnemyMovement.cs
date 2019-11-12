05b9b8246f343ac8ec5e84a91bdfb5c37e16373d
FabioKathriner <fabio-kathriner@windowslive.com>
2019-11-12 22:16:46 +0100

    Added some basic Waypoints for Enemies to follow. Still WIP

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Transform[] Waypoints;
+    private int NextWaypoint = 0;
+    private NavMeshAgent enemyAgent;
+    //public static Transform[] WaypointsNew;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyAgent = GetComponent<NavMeshAgent>();
+        enemyAgent.autoBraking = false;
+
+        GoToNextWaypoint();
+
+    }
+
+    
+
+    void GoToNextWaypoint()
+    {
+       if (Waypoints.Length == 0)
+            return;
+
+       enemyAgent.destination = Waypoints[NextWaypoint].position;
+        
+       NextWaypoint = (NextWaypoint + 1);
+
+       if (NextWaypoint == Waypoints.Length)
+       {
+           Destroy(gameObject);
+       }
+
+
+
+    }
+
+    /*
+    void GoToNextWaypointTest()
+    {
+        WaypointsNew = new Transform[transform.childCount];
+        //Always get the next Waypoint.
+        for (int i = 0; i < WaypointsNew.Length; i++)
+        {
+            WaypointsNew[i] = transform.GetChild(i);
+        }
+    }
+    */
+
+    void Update()
+    {
+        if(!enemyAgent.pathPending && enemyAgent.remainingDistance < 0.5f)
+            GoToNextWaypoint();
+    }
+}
+
+    
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointsClass : MonoBehaviour
+    //Work in Progress
+{
+
+    public static Transform[] Waypoints;
+
+
+    void Awake()
+    {
+        Waypoints = new Transform[transform.childCount];
+        //Always get the next Waypoint.
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            Waypoints[i] = transform.GetChild(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
