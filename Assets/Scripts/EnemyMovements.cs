623a2fc8c6134734e8ba80ab708666338f707715
TedMANNERs <mannersofted@gmail.com>
2019-11-14 17:35:52 +0100

    Fix nav agent errors on other scenes

--- Assets/Scripts/EnemyMovements.cs
+++ Assets/Scripts/EnemyMovements.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMovements : MonoBehaviour
 {
-    private Transform target;
-    private int waypointIndex = 0;
-    private NavMeshAgent enemyAgent;
+    private Transform _target;
+    private int _waypointIndex = 0;
+    private NavMeshAgent _enemyAgent;
     
     // Start is called before the first frame update
     void Start()
     {
-        target = Waypoints.points[0];
-        enemyAgent = GetComponent<NavMeshAgent>();
-        enemyAgent.autoBraking = false;
-        enemyAgent.destination = target.position;
+        var points = Waypoints.points;
+        if (points == null)
+            return;
+
+        _target = points[0];
+        _enemyAgent = GetComponent<NavMeshAgent>();
+        _enemyAgent.enabled = true;
+        _enemyAgent.autoBraking = false;
+        _enemyAgent.destination = _target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!enemyAgent.pathPending && enemyAgent.remainingDistance < 0.5f)
+        if (_enemyAgent != null && !_enemyAgent.pathPending && _enemyAgent.remainingDistance < 0.5f)
         {
             GetNextWaypoint();
         }
@@ -30,15 +36,15 @@ public class EnemyMovements : MonoBehaviour
 
     void GetNextWaypoint()
     {
-        waypointIndex++;
+        _waypointIndex++;
 
-        if (waypointIndex >= Waypoints.points.Length)
+        if (_waypointIndex >= Waypoints.points.Length)
         {
             Destroy(gameObject);
             return;
         }
 
-        enemyAgent.SetDestination(Waypoints.points[waypointIndex].position) ;
+        _enemyAgent.SetDestination(Waypoints.points[_waypointIndex].position) ;
         
     }
 }
