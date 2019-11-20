7162e859c835f4b61f1c7ed2cae9b040acb89bcd
FabioKathriner <fabio-kathriner@windowslive.com>
2019-11-20 21:50:21 +0100

    Added basic CameraController Script. Rotation will need to be implemented at a later point in time.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 30f;
+    public float panBoarderThickness = 10f;
+
+    private bool moveCamera = true;
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+
+    //TODO Camera Rotation
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            moveCamera = !moveCamera;
+        if (!moveCamera)
+            return;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBoarderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBoarderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        } 
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBoarderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBoarderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+        pos.y -= scroll * 500 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
+    }
+}
