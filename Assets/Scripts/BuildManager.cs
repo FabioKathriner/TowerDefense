826ab61ccc44e97789043973eae4f80a233f52e7
FabioKathriner <fabio-kathriner@windowslive.com>
2019-11-20 20:38:43 +0100

    Added basic turret placing. WIP raycast doesn't work as intended.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("There is more than one BuildManager in the current scene!");
+        }
+        instance = this;
+    }
+
+    public GameObject basicTurretPrefab;
+
+    void Start()
+    {
+        _turretToBuild = basicTurretPrefab;
+    }
+
+    private GameObject _turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return _turretToBuild;
+    }
+  
+}
---
+++ Assets/Scripts/GroundPlane.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class GroundPlane : MonoBehaviour
+{
+
+    private GameObject turret;
+    public LayerMask Mask;
+    //public LayerMask IgnoreLayer;
+    //public Camera RayCastCamera;
+    void OnMouseDown()
+    {
+        //Build a turret
+        GameObject _turretToBuild = BuildManager.instance.GetTurretToBuild();
+        
+        Vector3 mousePos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,0f);
+        Vector3 wordPos;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000f, Mask))
+        {
+            wordPos = hit.point;
+            Debug.LogWarning("Hitpoint found!");
+        }
+        else
+        {
+            wordPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Debug.LogWarning("no hitpoint Found");
+        }
+        turret = (GameObject)Instantiate(_turretToBuild, wordPos, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+            Debug.DrawLine(ray.origin,hit.point);
+    }
+}
