f58f931ef6768a636b6b2131ec8d789a4aa1a33d
FabioKathriner <fabio-kathriner@windowslive.com>
2019-11-22 13:37:51 +0100

    Fixed turret placing so they can't be put on each other.

--- Assets/Scripts/GroundPlane.cs
+++ Assets/Scripts/GroundPlane.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
 using UnityEngine.UIElements;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
@@ -6,21 +7,29 @@ using Vector3 = UnityEngine.Vector3;
 public class GroundPlane : MonoBehaviour
 {
 
-    private GameObject turret;
-    public LayerMask Mask;
+    private GameObject _turret;
+    public LayerMask HitMask;
+    public LayerMask IgnoreTowerMask;
 
     void FixedUpdate()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
+        RaycastHit nonhit;
 
-        if (Physics.Raycast(ray, out hit, 100, Mask))
-            Debug.DrawLine(ray.origin,hit.point);
+        if (Physics.Raycast(ray, out hit, 1000, HitMask))
+            Debug.DrawLine(ray.origin, hit.point);
+        
         if (Input.GetMouseButtonDown(0))
         {
-
+            if (Physics.Raycast(ray, out nonhit, 1000, IgnoreTowerMask))
+            {
+                Debug.LogWarning("Can't place turret on top of another turret!'");
+                return;
+            }
             GameObject _turretToBuild = BuildManager.instance.GetTurretToBuild();
-            turret = (GameObject)Instantiate(_turretToBuild, hit.point, Quaternion.identity);
+            _turret = (GameObject)Instantiate(_turretToBuild, hit.point, Quaternion.identity);
+            Debug.LogWarning("Turret Placed successfully");
         }
     }
 }
