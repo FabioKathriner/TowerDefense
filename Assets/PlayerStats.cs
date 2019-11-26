59df2e8726df98d4b33f5c197861a6a666ef0707
FabioKathriner <fabio-kathriner@windowslive.com>
2019-11-26 22:04:52 +0100

    Implemented Basic Money System for Towers.

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int Money;
+    public int startMoney = 1000;
+
+    void Start()
+    {
+        Money = startMoney;
+    }
+}
--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Quaternion = UnityEngine.Quaternion;
 
 public class BuildManager : MonoBehaviour
 {
     public static BuildManager instance;
+
+    public LayerMask HitMask;
+    public LayerMask IgnoreTowerMask;
+
+    public GameObject RocketTowerPrefab;
+    public GameObject BallisticTowerPrefab;
+    public GameObject GuidedMissileTowerPrefab;
+    public GameObject BlastTowerPrefab;
+
+    private TurretBlueprint _turretToBuild;
+    private BuildManager buildManager;
+    private GameObject _turret;
 
     void Awake()
     {
@@ -15,21 +30,55 @@ public class BuildManager : MonoBehaviour
         instance = this;
     }
 
-    public GameObject RocketTowerPrefab;
-    public GameObject BallisticTowerPrefab;
-    public GameObject GuidedMissileTowerPrefab;
-    public GameObject BlastTowerPrefab;
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
     
-    private GameObject _turretToBuild;
+    //Property to check if _turretToBuild is set
+    public bool CanBuild {get {return _turretToBuild != null; }}
+    
 
-    public GameObject GetTurretToBuild()
-    {
-        return _turretToBuild;
+    public void SelectTurretToBuild (TurretBlueprint turretBlueprint)
+    { 
+      _turretToBuild = turretBlueprint;
     }
 
-    public void SetTurretToBuild(GameObject turret)
+    void FixedUpdate()
     {
-        _turretToBuild = turret;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        RaycastHit nonhit;
+
+        if (Physics.Raycast(ray, out hit, 1000, HitMask))
+          //  Debug.DrawLine(ray.origin, hit.point);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
+
+            if (!buildManager.CanBuild)
+                return;
+
+            if (Physics.Raycast(ray, out nonhit, 1000, IgnoreTowerMask))
+            {
+                Debug.LogWarning("Can't place a Turret on top of another Turret!'");
+                return;
+            }
+
+            if (PlayerStats.Money < _turretToBuild.turretCost)
+            {
+                Debug.LogWarning("You don't have enough Money to build that turret!'");
+                return;
+            }
+
+            PlayerStats.Money -= _turretToBuild.turretCost;
+
+            GameObject turret = (GameObject)Instantiate(_turretToBuild.prefab, hit.point, Quaternion.identity);
+            Debug.LogWarning("Turret Placed successfully");
+            Debug.Log("Turret built! Money left: " + PlayerStats.Money);
+        }
     }
-  
+
 }
