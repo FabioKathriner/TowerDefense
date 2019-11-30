8af22ee1d7f8387ef4c8520c001cc3ef08f24a01
TedMANNERs <mannersofted@gmail.com>
2019-11-30 17:24:50 +0100

    Add tower placement preview

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -11,77 +11,85 @@ namespace Assets.Scripts
         public LayerMask HitMask;
         public LayerMask IgnoreTowerMask;
 
-        public GameObject RocketTowerPrefab;
-        public GameObject BallisticTowerPrefab;
-        public GameObject GuidedMissileTowerPrefab;
-        public GameObject BlastTowerPrefab;
-
         private TurretBlueprint _turretToBuild;
         private BuildManager buildManager;
-        private GameObject _turret;
 
         private PlayerStats _playerStats;
+        private bool _IsInBuildMode;
+        private GameObject _selectedTowerPreview;
+        private GameObject _selectedTowerPrefab;
 
-        void Awake()
+        private void Awake()
         {
             if (instance != null)
-            {
                 Debug.LogError("There is more than one BuildManager in the current scene!");
-            }
             instance = this;
         }
 
-        void Start()
+        private void Start()
         {
             _playerStats = GetComponent<PlayerStats>();
             buildManager = BuildManager.instance;
         }
-    
-        //Property to check if _turretToBuild is set
-        public bool CanBuild {get {return _turretToBuild != null; }}
-    
 
         public void SelectTurretToBuild (TurretBlueprint turretBlueprint)
         { 
             _turretToBuild = turretBlueprint;
         }
 
-        void FixedUpdate()
+        private void FixedUpdate()
         {
+            if (!_IsInBuildMode)
+                return;
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                _IsInBuildMode = false;
+                Destroy(_selectedTowerPreview);
+                _selectedTowerPrefab = null;
+                _selectedTowerPreview = null;
+                return;
+            }
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             RaycastHit nonhit;
+            if (!Physics.Raycast(ray, out hit, 1000, HitMask))
+                return;
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
 
-            if (Physics.Raycast(ray, out hit, 1000, HitMask))
-                //  Debug.DrawLine(ray.origin, hit.point);
+            // TODO: second raycast needed?
+            if (Physics.Raycast(ray, out nonhit, 1000, IgnoreTowerMask))
+            {
+                Debug.LogWarning("Can't place a Turret on top of another Turret!'");
+                return;
+            }
 
-                if (Input.GetMouseButtonDown(0))
+            if (_selectedTowerPreview != null)
+                _selectedTowerPreview.transform.position = hit.point;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (_playerStats.Money < _turretToBuild.turretCost)
                 {
-                    if (EventSystem.current.IsPointerOverGameObject())
-                        return;
-
-                    if (!buildManager.CanBuild)
-                        return;
-
-                    if (Physics.Raycast(ray, out nonhit, 1000, IgnoreTowerMask))
-                    {
-                        Debug.LogWarning("Can't place a Turret on top of another Turret!'");
-                        return;
-                    }
-
-                    if (_playerStats.Money < _turretToBuild.turretCost)
-                    {
-                        Debug.LogWarning("You don't have enough Money to build that turret!'");
-                        return;
-                    }
-
-                    _playerStats.Money -= _turretToBuild.turretCost;
-
-                    GameObject turret = (GameObject)Instantiate(_turretToBuild.prefab, hit.point, Quaternion.identity);
-                    Debug.LogWarning("Turret Placed successfully");
-                    Debug.Log("Turret built! Money left: " + _playerStats.Money);
+                    Debug.LogWarning("You don't have enough Money to build that turret!'");
+                    return;
                 }
+
+                _playerStats.Money -= _turretToBuild.turretCost;
+                Destroy(_selectedTowerPreview);
+                GameObject turret = Instantiate(_selectedTowerPrefab, hit.point, Quaternion.identity);
+                Debug.LogWarning("Turret Placed successfully");
+                Debug.Log("Turret built! Money left: " + _playerStats.Money);
+            }
         }
 
+        public void EnterBuildMode(GameObject previewPrefab, GameObject actualPrefab)
+        {
+            _selectedTowerPreview = Instantiate(previewPrefab);
+            _selectedTowerPrefab = actualPrefab;
+            _IsInBuildMode = true;
+        }
     }
 }
---
+++ Assets/Scripts/TowerBuildButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class TowerBuildButton : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _previewPrefab;
+
+        [SerializeField]
+        private GameObject _actualPrefab;
+
+        [SerializeField]
+        private BuildManager _buildManager;
+
+        private void Start()
+        {
+            var button = GetComponent<Button>();
+            button.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            _buildManager.EnterBuildMode(_previewPrefab, _actualPrefab);
+        }
+    }
+}
