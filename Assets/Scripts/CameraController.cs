c1742c81a4287a2315d448f283b1e6f16fbd20e0
FabioKathriner <fabio-kathriner@windowslive.com>
2019-12-01 22:14:20 +0100

    Added Camera Rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,45 +1,85 @@
-﻿using TMPro;
+﻿using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float panSpeed = 30f;
-    public float panBoarderThickness = 10f;
+    [SerializeField] private float panSpeed = 30f;
 
     private bool moveCamera = true;
-    public float scrollSpeed = 5f;
-    public float minY = 10f;
-    public float maxY = 80f;
 
-    //TODO Camera Rotation
+    [SerializeField] private float scrollSpeed = 5f;
+
+    [Header("CameraLimits")] [SerializeField]
+    private float minY = 10f;
+
+    [SerializeField] private float maxY = 80f;
+    [SerializeField] private float rotateSpeed = 10; // mouse down rotation speed about x and y axess
+
+    private Vector3 _lastMousePosition;
 
     // Update is called once per frame
     void LateUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             moveCamera = !moveCamera;
+
         if (!moveCamera)
             return;
 
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBoarderThickness)
+        if (Input.GetKey("w") || (Input.GetKey("up")))
         {
-            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.Self);
         }
 
-        if (Input.GetKey("s") || Input.mousePosition.y <= panBoarderThickness)
+        if (Input.GetKey("s") || Input.GetKey("down"))
         {
-            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
-        } 
-
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBoarderThickness)
-        {
-            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.Self);
         }
 
-        if (Input.GetKey("a") || Input.mousePosition.x <= panBoarderThickness)
+        if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.Self);
         }
+
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            // if the game window is separate from the editor window and the editor
+            // window is active then you go to right-click on the game window the
+            // rotation jumps if  we don't ignore the mouseDelta for that frame.
+            Vector3 mouseDelta;
+            if (_lastMousePosition.x >= 0 &&
+                _lastMousePosition.y >= 0 &&
+                _lastMousePosition.x <= Screen.width &&
+                _lastMousePosition.y <= Screen.height)
+                mouseDelta = Input.mousePosition - _lastMousePosition;
+            else
+                mouseDelta = Vector3.zero;
+
+            var rotation = Vector3.up * Time.deltaTime * rotateSpeed * mouseDelta.x;
+            rotation.x = 0;
+            rotation.z = 0;
+            transform.Rotate(rotation, Space.Self);
+
+            // Make sure z rotation stays locked
+            rotation = transform.rotation.eulerAngles;
+            transform.rotation = Quaternion.Euler(rotation);
+
+
+            var cameraRotation = Vector3.left * Time.deltaTime * rotateSpeed * mouseDelta.y;
+            cameraRotation.y = 0;
+            cameraRotation.z = 0;
+            Camera.main.transform.Rotate(cameraRotation, Space.Self);
+
+            cameraRotation = Camera.main.transform.rotation.eulerAngles;
+            Camera.main.transform.rotation = Quaternion.Euler(cameraRotation);
+        }
+
+        _lastMousePosition = Input.mousePosition;
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
