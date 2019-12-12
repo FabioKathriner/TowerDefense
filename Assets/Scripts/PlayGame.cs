2b2ba051f358d671ff97e47141b291cee8867ca7
TedMANNERs <mannersofted@gmail.com>
2019-12-12 16:24:11 +0100

    Load LevelTest2 on clicking play

--- Assets/Scripts/PlayGame.cs
+++ Assets/Scripts/PlayGame.cs
@@ -1,21 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-		
-	}
-    void Load()
+    public void Load()
 	{
-		Application.LoadLevel("Level Test 2");
-	}
-    // Update is called once per frame
-    void Update()
-    {
-		
+		SceneManager.LoadScene("Level Test 2");
 	}
 }
