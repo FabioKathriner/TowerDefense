35ce6598aa0ab8eb201f07887d3549e4f95e8003
FabioKathriner <fabio-kathriner@windowslive.com>
2019-12-12 18:15:09 +0100

    Added Time Scripts and Base Prefab.

---
+++ Assets/ChangeTimeButton.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTimeButton : MonoBehaviour
+{
+    private float fixedDeltaTime;
+
+    void Awake()
+    {
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    public void PauseResumeGame()
+    {
+        if (Time.timeScale == 1.0f)
+        {
+            Time.timeScale = 0f;
+        }
+        else if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1.0f;
+        }
+        else if (Time.timeScale != 1.0f)
+        {
+            Time.timeScale = 0f;
+        }
+        
+
+        // Adjust fixed delta time according to timescale
+        // The fixed delta time will now be 0.02 frames per real-time second
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+
+    }
+
+    public void GoFast()
+    {
+        Time.timeScale = 1.5f;
+       
+        // Adjust fixed delta time according to timescale
+        // The fixed delta time will now be 0.02 frames per real-time second
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+    }
+
+    public void GoVeryFast()
+    {
+      
+        Time.timeScale = 2.0f;
+        // Adjust fixed delta time according to timescale
+        // The fixed delta time will now be 0.02 frames per real-time second
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+    }
+
+    public void SlowDown()
+    {
+       
+        Time.timeScale = 0.5f;
+        // Adjust fixed delta time according to timescale
+        // The fixed delta time will now be 0.02 frames per real-time second
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+    }
+
+    public void SlowDownEvenMore()
+    {
+        
+        Time.timeScale = 0.25f;
+        // Adjust fixed delta time according to timescale
+        // The fixed delta time will now be 0.02 frames per real-time second
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+    }
+}
---
+++ Assets/TimeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    private float fixedDeltaTime;
+    void Awake()
+    {
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            if (Time.timeScale == 1.0f)
+                Time.timeScale = 0.0f;
+            else
+                Time.timeScale = 1.0f;
+            // Adjust fixed delta time according to timescale
+            // The fixed delta time will now be 0.02 frames per real-time second
+            Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+        }
+
+    }
+}
