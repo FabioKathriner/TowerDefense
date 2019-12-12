64e102493e9db4047e43d08a0420030872721b38
pevie <pedro.vie@hotmail.com>
2019-12-12 13:20:48 +0100

    onClick Try

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+	}
+    void OnClick()
+	{
+		Application.LoadLevel("Level Test 2");
+	}
+    // Update is called once per frame
+    void Update()
+    {
+		
+	}
+}
