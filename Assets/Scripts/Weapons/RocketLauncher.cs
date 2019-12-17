8c9fb8445460667d7f652b6ba6c4e0360f022431
TedMANNERs <mannersofted@gmail.com>
2019-12-18 00:59:14 +0100

    Add animation to rocket tower

--- Assets/Scripts/Weapons/RocketLauncher.cs
+++ Assets/Scripts/Weapons/RocketLauncher.cs
@@ -5,6 +5,14 @@ namespace Assets.Scripts.Weapons
 {
     public class RocketLauncher : ProjectileWeapon
     {
+        private Animator _animation;
+
+        protected override void Start()
+        {
+            base.Start();
+            _animation = GetComponentInChildren<Animator>();
+        }
+
         public override void Fire(GameObject target)
         {
             if (target == null)
@@ -13,6 +21,7 @@ namespace Assets.Scripts.Weapons
             transform.LookAt(target.transform);
             var projectile = Instantiate(ProjectilePrefab, _spawnPointObject.transform.position, transform.rotation);
             var script = projectile.GetComponent<Rocket>();
+            _animation.SetTrigger("Fire");
             if (script != null)
                 script.Target = target;
         }
