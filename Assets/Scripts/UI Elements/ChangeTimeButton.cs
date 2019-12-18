54e09047fdfda7d1383525757991d951501759d0
TedMANNERs <mannersofted@gmail.com>
2019-12-18 23:22:04 +0100

    Update pause resume button

--- Assets/Scripts/UI Elements/ChangeTimeButton.cs
+++ Assets/Scripts/UI Elements/ChangeTimeButton.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.UI_Elements
 {
     public class ChangeTimeButton : MonoBehaviour
     {
+        [SerializeField]
+        private Image _pauseResumeImage;
+        [SerializeField]
+        private Sprite _playImage;
+        [SerializeField]
+        private Sprite _pauseImage;
+
         private TimeManager _timeManager;
 
         private void Awake()
@@ -14,26 +22,31 @@ namespace Assets.Scripts.UI_Elements
         public void OnPauseResume()
         {
             _timeManager.PauseResume();
+            _pauseResumeImage.sprite = _timeManager.GameSpeed == GameSpeed.Paused ? _playImage : _pauseImage;
         }
 
         public void OnGoFast()
         {
             _timeManager.GoFast();
+            _pauseResumeImage.sprite = _pauseImage;
         }
 
         public void OnGoVeryFast()
         {
             _timeManager.GoVeryFast();
+            _pauseResumeImage.sprite = _pauseImage;
         }
 
         public void OnGoSlow()
         {
             _timeManager.GoSlow();
+            _pauseResumeImage.sprite = _pauseImage;
         }
 
         public void OnGoVerySlow()
         {
             _timeManager.GoVerySlow();
+            _pauseResumeImage.sprite = _pauseImage;
         }
     }
 }
